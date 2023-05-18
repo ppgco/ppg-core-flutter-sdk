dfb1eda40c370155cf4eb723ec6ea19bcc1d30af
worotyns <mateusz@pushpushgo.com>
2023-05-18 16:11:48 +0200

    new version of ios + plist

--- example/ios/NSE/NotificationService.swift
+++ example/ios/NSE/NotificationService.swift
@@ -9,7 +9,5 @@ import UserNotifications
 import PpgCoreSDK
 
 class NotificationService: PpgCoreNotificationServiceExtension {
-  override func getEndpoint() -> String {
-    return "https://ppg-core.master1.qappg.co/v1"
-  }
+
 }

49e5fa3070be0025b88a2ae8581ad77a87dd3a17
worotyns <mateusz@pushpushgo.com>
2023-05-23 09:39:16 +0200

    fix: add deps to android

--- android/src/main/kotlin/com/pushpushgo/ppg_core/PpgCorePlugin.kt
+++ android/src/main/kotlin/com/pushpushgo/ppg_core/PpgCorePlugin.kt
@@ -7,6 +7,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import com.pushpushgo.core_sdk.sdk.client.PpgCoreClient
 
 /** PpgCorePlugin */
 class PpgCorePlugin: FlutterPlugin, MethodCallHandler {
--- example/android/app/src/main/kotlin/com/example/ppg_core_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/ppg_core_example/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.pushpushgo.ppg_core_example
 
 import io.flutter.embedding.android.FlutterActivity
-
 class MainActivity: FlutterActivity() {
+
 }
